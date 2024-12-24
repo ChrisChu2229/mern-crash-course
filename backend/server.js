876f1bb8e671f2d14940da0f34ffba02de6b15d3
Christopher Chu <christopherchu@Christophers-Mac-mini.local>
2024-12-24 09:59:02 -0800

    adding frontend

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,13 @@ import productRoutes from "./routes/product.routes.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json()); // allows us to accept JSON data in the req.body (middleware - function that runs before you send a response back to the client)
 
 app.use("/api/products", productRoutes);
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:4000")
+    console.log("Server started at http://localhost:" + PORT);
 });
